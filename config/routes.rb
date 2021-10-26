17ee90ca3df8ba1d9fddd083fcd811867373844d
juanf <jfruizg@unbosque.edu.co>
2021-10-26 10:15:43 -0500

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :announcements
+  devise_for :users
   get 'index', to: 'announcements#index'
   get 'home', to: 'announcements#home'
   get 'user', to: 'announcements#user'
   root 'announcements#home'
-  devise_for :users
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
