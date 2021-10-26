d02cec2f1f4d8d74e51d3ff2611b5e0fc7769511
juanf <jfruizg@unbosque.edu.co>
2021-10-26 09:48:53 -0500

    UPDATE routes and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'announcements/index'
+  resources :announcements
+  get 'index', to: 'announcements#index'
+  get 'home', to: 'announcements#home'
+  get 'user', to: 'announcements#user'
+  root 'announcements#home'
   devise_for :users
-  get 'proyecto_1/index_proyecto1'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
