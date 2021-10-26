2ca20b576de56ca5376df98f388faabffe51f077
juanf <jfruizg@unbosque.edu.co>
2021-10-26 10:04:11 -0500

    UPDATE views, register

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_143630) do
+ActiveRecord::Schema.define(version: 2021_10_26_144310) do
 
   create_table "announcements", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_143630) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_announcements_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_10_26_143630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "announcements", "users"
 end
